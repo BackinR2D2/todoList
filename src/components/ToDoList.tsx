fb6f6432c18c1a70ca60403b834954f8753151b7
Cristian Niculae <cristimlg334@gmail.com>
2023-10-26 21:18:19 +0300

    Added copy to clipboard functionality

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,8 +1,12 @@
-import { Box } from '@chakra-ui/react';
+import { Box, IconButton, useToast } from '@chakra-ui/react';
 import { ToDoProps } from '../types/ToDoProps';
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { CopyIcon } from '@chakra-ui/icons';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 function ToDoList(props: ToDoProps) {
+
+    const toast = useToast();
 
     const handleDrop = (droppedItem: any) => {
         if (!droppedItem.destination) return;
@@ -10,6 +14,17 @@ function ToDoList(props: ToDoProps) {
         const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1);
         updatedList.splice(droppedItem.destination.index, 0, reorderedItem);
         props.setTodos(updatedList);
+    };
+
+    const handleCopyToClipboardCopy = (item: string) => {
+        toast({
+            title: 'Copied to Clipboard',
+            description: `Element: ${item} has been copied successfully`,
+            status: 'success',
+            duration: 1500,
+            isClosable: true,
+            position: 'top-right',
+        });
     };
 
     return (
@@ -20,9 +35,14 @@ function ToDoList(props: ToDoProps) {
                         <div className='list-container'>
                             {
                                 props.searchResults.map((item, index) => (
-                                    <div className='item-container' key={index}>
-                                        {item}
-                                    </div>
+                                    <>
+                                        <div className='item-container' key={index}>
+                                            {item}
+                                            <CopyToClipboard text={item} onCopy={() => handleCopyToClipboardCopy(item)}>
+                                                <IconButton aria-label='Copy To Clipboard' icon={<CopyIcon />} />
+                                            </CopyToClipboard>
+                                        </div>
+                                    </>
                                 ))
                             }
                         </div>
@@ -30,27 +50,34 @@ function ToDoList(props: ToDoProps) {
                         <DragDropContext onDragEnd={handleDrop}>
                             <Droppable droppableId="list-container">
                             {(provided) => (
-                                <div
-                                className="list-container"
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                                >
-                                    {props.todos.map((item, index) => (
-                                        <Draggable key={`${item}${index}`} draggableId={`${item}${index}`} index={index}>
-                                        {(provided) => (
-                                            <div
-                                            className="item-container"
+                                    <>
+                                        <div
+                                            className="list-container"
+                                            {...provided.droppableProps}
                                             ref={provided.innerRef}
-                                            {...provided.dragHandleProps}
-                                            {...provided.draggableProps}
-                                            >
-                                            {item}
-                                            </div>
-                                        )}
-                                        </Draggable>
-                                    ))}
-                                    {provided.placeholder}
-                                    </div>
+                                        >
+                                            {props.todos.map((item, index) => (
+                                                <Draggable key={`${item}${index}`} draggableId={`${item}${index}`} index={index}>
+                                                {(provided) => (
+                                                    <>
+                                                        <div
+                                                            className="item-container"
+                                                            ref={provided.innerRef}
+                                                            {...provided.dragHandleProps}
+                                                            {...provided.draggableProps}
+                                                            >
+                                                            {item}
+                                                            <CopyToClipboard text={item} onCopy={() => handleCopyToClipboardCopy(item)}>
+                                                                <IconButton aria-label='Copy To Clipboard' icon={<CopyIcon />} />
+                                                            </CopyToClipboard>
+                                                        </div>
+                                                    </>
+                                                )}
+                                                </Draggable>
+                                            ))}
+                                            {provided.placeholder}
+                                        </div>
+                                    </>
                                 )}
                             </Droppable>
                         </DragDropContext>
